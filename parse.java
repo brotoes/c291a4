f39f559e30e41c8b2606855cdc97c33ac51b19fa
Lyle Rolleman <lrollema@cs.ualberta.ca>
2012-11-21 21:10:19 -0700

    modded parse to load song database

--- parse.java
+++ parse.java
@@ -34,6 +34,7 @@ public class parse {
       //    recordDB.artists_DB.close();
       //recordDB.userrat_DB.close();
     }
+    SongDatabase.init();
     Record.populateDatabase();
     System.out.println("Done, database populated");
     //if (args.length == 2)
@@ -46,13 +47,14 @@ public class parse {
     //  recordtest.testQ();
     //linSearch.linSearch();
     //Record record = new Record(1, new String(), new ArrayList<String>(), new ArrayList<String>(), new ArrayList<Integer>());
-    /*
+    linSearch.linSearch();
+    indexSearch.indSearch();
     long[] times = getTimeData();
     System.out.println("The min linear time was " + times[0] + " the max was " + times[1] +
                        " The average was " + times[2]);
     System.out.println("The min index time was " + times[3] + " The max was " + times[4] +
                        " The average was " + times[5]);
-    */
+    
     sc.close();
     } catch (FileNotFoundException e) {
       System.err.print("FileNotFoundException: ");
