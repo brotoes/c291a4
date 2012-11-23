999dad5aa394c501303c7faf830bcc25017f1d69
Lyle Rolleman <lrollema@cs.ualberta.ca>
2012-11-23 10:06:20 -0700

    added data file, detects problem in indexed search

--- indexSearch.java
+++ indexSearch.java
@@ -6,33 +6,40 @@ public class indexSearch {
   //private static ArrayList<Double> Scores = new ArrayList<Double>();
   
   public static void indSearch() {
+    try {
+      FileWriter fwrite = new FileWriter("indexedanswers.txt");
+      BufferedWriter writer = new BufferedWriter(fwrite);
     
-    for (int i=0; i<Record.numQuery(); i++) {
-      timer.startIndTimer();
-      int id = Integer.parseInt(Record.getQuery(i));
-      ArrayList<Entry> allEntries = new ArrayList<Entry>();
-      
-      Entry entry = SongDatabase.getEntry(id);
-      ArrayList<Entry> aentry = new ArrayList<Entry>();
-      
-      for (int j=0; j<entry.user.size(); j++) {
-        aentry = SongDatabase.getEntry(entry.user.get(j));
-        for (int k=0; k<aentry.size(); k++) {
-          if (!allEntries.contains(aentry.get(k)) && aentry.get(k).songID != id)
+    
+      for (int i=0; i<Record.numQuery(); i++) {
+        timer.startIndTimer();
+        int id = Integer.parseInt(Record.getQuery(i));
+        ArrayList<Entry> allEntries = new ArrayList<Entry>();
+        
+        Entry entry = SongDatabase.getEntry(id);
+        ArrayList<Entry> aentry = new ArrayList<Entry>();
+        
+        for (int j=0; j<entry.user.size(); j++) {
+          aentry = SongDatabase.getEntry(entry.user.get(j));
+          for (int k=0; k<aentry.size(); k++) {
+            if (!allEntries.contains(aentry.get(k)) && aentry.get(k).songID != id)
             allEntries.add(aentry.get(k));
+          }
         }
-      }
-      
-      for (int j=0; j<allEntries.size(); j++) {
-        double score = -1.0;
-        score = linSearch.compareEntry(entry, allEntries.get(j));
-        linSearch.IDs.add(allEntries.get(j).songID);
-        linSearch.Scores.add(new Double(score));
-      }
-      
-      linSearch.getBest(id, "i");
-      timer.stopIndTimer();
-    } //end query for
-    
+        
+        for (int j=0; j<allEntries.size(); j++) {
+          double score = -1.0;
+          score = linSearch.compareEntry(entry, allEntries.get(j));
+          linSearch.IDs.add(allEntries.get(j).songID);
+          linSearch.Scores.add(new Double(score));
+        }
+        
+        linSearch.getBest(id, writer);
+        timer.stopIndTimer();
+      } //end query for
+      writer.close();
+    } catch (Exception e) {
+      e.getMessage();
+    }
   }
 }
--- linSearch.java
+++ linSearch.java
@@ -5,37 +5,42 @@ public class linSearch {
   public static ArrayList<Integer> IDs = new ArrayList<Integer>();
   public static ArrayList<Double> Scores = new ArrayList<Double>();
   
-  public static void linSearch() {
-    
-    int id = -1;
-    
-    for (int i=0; i<Record.numQuery(); i++) {
-      //System.out.println("marker");
-      timer.startLinTimer();
-      
-      id = Integer.parseInt(Record.getQuery(i));
-      Entry init = SongDatabase.getEntry(id);
-      
-      for (int j=1; j<=Record.recordSize(); j++) {
-        Entry comp = SongDatabase.getEntry(j);
-        //System.out.println(init.user.get(0));
-        double score = -1.0;
-        if (init.songID != comp.songID) 
-          score = compareEntry(init, comp);
-        if (score >= 0) {
-          IDs.add(new Integer(j));
-          Scores.add(new Double(score));
-          System.out.println(j + " " + score);
-        }
-            //System.out.println(score);
-           
-      }
-      timer.stopLinTimer();
-      getBest(id, "l");
-    }
   
+  public static void linSearch() {
+    try {
+      FileWriter fwrite = new FileWriter("linearanswers.txt");
+      BufferedWriter writer = new BufferedWriter(fwrite);
     
-    
+      int id = -1;
+      
+      for (int i=0; i<Record.numQuery(); i++) {
+        //System.out.println("marker");
+        timer.startLinTimer();
+        
+        id = Integer.parseInt(Record.getQuery(i));
+        Entry init = SongDatabase.getEntry(id);
+        
+        for (int j=1; j<=Record.recordSize(); j++) {
+          Entry comp = SongDatabase.getEntry(j);
+          //System.out.println(init.user.get(0));
+          double score = -1.0;
+          if (init.songID != comp.songID) 
+            score = compareEntry(init, comp);
+          if (score >= 0) {
+            IDs.add(new Integer(j));
+            Scores.add(new Double(score));
+            //System.out.println(j + " " + score);
+          }
+          //System.out.println(score);
+          
+        }
+        timer.stopLinTimer();
+        getBest(id, writer);
+      }
+      writer.close();
+    } catch (Exception e) {
+      e.getMessage();
+    }
   }
   
   public static double compareEntry(Entry entry1, Entry entry2) {
@@ -69,7 +74,7 @@ public class linSearch {
     return dresult;
   }
 
-  public static void getBest(int id, String type) {
+  public static void getBest(int id, BufferedWriter writer) {
     Integer bid = new Integer(-1);
     Integer sid = new Integer(-1);
     Integer tid = new Integer(-1);
@@ -106,28 +111,13 @@ public class linSearch {
       }
     }
     String output = new Integer(id).toString().concat(" ")
-      .concat(bid.toString()).concat(" ").concat(sid.toString()).concat(" ").concat(tid.toString());
-    //System.out.println("The top similarities to " + id + " are " + bid + " " + sid + " " + tid);
-    if (type.equals("l")) {
-      try {
-        FileWriter fwrite = new FileWriter("linearanswers.txt");
-        BufferedWriter writer = new BufferedWriter(fwrite);
-        
-        writer.write(output);
-        writer.close();
-      } catch (Exception e) {
-        e.getMessage();
-      }
-    } else {
-      try {
-        FileWriter fwrite = new FileWriter("indexedanswers.txt");
-        BufferedWriter writer = new BufferedWriter(fwrite);
-        writer.write(output);
-        writer.close();
-      } catch (Exception e) {
-        e.getMessage();
-      }
-      
+      .concat(bid.toString()).concat(" ").concat(sid.toString()).concat(" ").concat(tid.toString().concat("\n"));
+    System.out.println("The top similarities to " + id + " are " + bid + " " + sid + " " + tid);
+    
+    try {
+      writer.write(output);
+    } catch (Exception e) {
+      e.getMessage();
     }
     IDs.clear();
     Scores.clear();
